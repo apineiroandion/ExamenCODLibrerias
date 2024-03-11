29cee8f95e4f28a2b62d4a5e5ae0bca03da47512
Angel <apineiroandion@danielcastelao.org>
2024-03-11 09:31:38 +0100

    añado funcion raiz

--- src/Calculadora/Operacion.java
+++ src/Calculadora/Operacion.java
@@ -5,6 +5,7 @@ public class Operacion {
     public static final int RESTA = 1;
     public static final int MULTIPLICACION = 2;
     public static final int DIVISION = 3;
+    public static final int RAIZ = 4;
 
     public static Float operar(Float num1, Float num2, int operacion){
         switch(operacion){
@@ -16,6 +17,8 @@ public class Operacion {
                 return num1 * num2;
             case DIVISION:
                 return num1 / num2;
+            case RAIZ:
+                return (float) Math.pow(num1, num2);
             default:
                 return null;
         }
